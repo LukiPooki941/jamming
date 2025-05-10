4fd3fe90c19e91f76378316057d00cafba7cf84f
Luke <lukecherniawski@gmail.com>
2025-05-09 23:20:12 -0600

    simplifying the process

---
+++ jammming/react_modules/App.jsx
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+return (
+    <div>
+     <head>
+        <link href='./app.css' rel='stylesheet'>
+        </link>
+     </head>
+     <body>
+        <searchBar />
+        <searchResults />
+        <playList />
+        <trackList />
+        <track />
+     </body>
+    </div>
+)
+}
+
+export default App;
